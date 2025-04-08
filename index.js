2a08fe3371e15f83d6356ca7abc3df69a23a5368
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-04-08 21:55:03 +0530

    done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const { Telegraf } = require('telegraf');
-require('dotenv').config();
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome to bharat\'s bot'))
-bot.command('hello', (ctx) => ctx.reply('Hello World!'))
-bot.on('sticker', (ctx) => ctx.reply('👍') )
-bot.command('bharat' , (ctx) => ctx.reply('i love you anu'))
-bot.launch() ;
+const { Telegraf } = require("telegraf");
+const { message } = require('telegraf/filters')
+require("dotenv").config();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => ctx.reply("Welcome to bharat's bot"));
+bot.command("hello", (ctx) => ctx.reply("Hello World!"));
+bot.on(('sticker'), (ctx) => ctx.reply('👍'))
+bot.command("bharat", (ctx) => ctx.reply("i love you anu"));
+
+bot.launch();
